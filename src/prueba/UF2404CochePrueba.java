1aedbea5b4a09d5d49d95cdf2c93f9e131e71581
Alejandro <alejandro1996rd@gmail.com>
2020-03-12 13:55:45 +0100

    Prueba 2 mejorada (incluye do while)

--- src/prueba/UF2404CochePrueba.java
+++ src/prueba/UF2404CochePrueba.java
@@ -14,54 +14,10 @@ public class UF2404CochePrueba {
 		System.out.println("¿cuantos coches desea ingresar?");
 		numeroCoches = Integer.parseInt(sc.nextLine());
 		Coche[] coches = new Coche[numeroCoches]; // Array que guardare mis nuevos coches
-		int contador = 1;
+		int contador = 0;
 
-//		do {
-//			contador = contador;
-//			Coche coche = new Coche();
-//			System.out.println("Si desea ingresar sus datos porfavor escriba 1, en caso contrario escriba 2");
-//			respuesta = Integer.parseInt(sc.nextLine());
-//
-//			switch (respuesta) {
-//
-//			case 1:
-//				System.out.println("Porfavor indique la matricula");
-//				coche.setMatricula(sc.nextLine());
-//
-//				System.out.println("Porfavor indique la marca");
-//				coche.setMarca(sc.nextLine());
-//
-//				System.out.println("Porfavor indique la modelo");
-//				coche.setModelo(sc.nextLine());
-//
-//				System.out.println("Porfavor indique la color");
-//				coche.setColor(sc.nextLine());
-//
-//				System.out.println("Porfavor indique la potencia");
-//				coche.setPotencia(Float.parseFloat(sc.nextLine()));
-//
-//				System.out.println("Porfavor indique la cilindrada");
-//				coche.setCilindrada(Float.parseFloat(sc.nextLine()));
-//
-//				coches[contador - 1] = coche;
-//
-//				contador++;
-//				break;
-//
-//			case 2:
-//				System.out.println("no se ingresaran los datos del coche " + (contador - 1));
-//				coches[contador] = coche;
-//				contador++;
-//				break;
-//			default:
-//				System.out.println("esa opcion no es valida, porfavor vuelva a escribir su opcion");
-//				contador = (contador - 1);
-//				break;
-//
-//			}
-//		} while (contador < (numeroCoches + 1));
-
-		for (int i = 0; i < coches.length; i++) {
+		do {
+			contador = contador;
 			Coche coche = new Coche();
 			System.out.println("Si desea ingresar sus datos porfavor escriba 1, en caso contrario escriba 2");
 			respuesta = Integer.parseInt(sc.nextLine());
@@ -87,19 +43,68 @@ public class UF2404CochePrueba {
 				System.out.println("Porfavor indique la cilindrada");
 				coche.setCilindrada(Float.parseFloat(sc.nextLine()));
 
-				coches[i] = coche;
+				coches[contador] = coche;
+
+				contador++;
 				break;
 
 			case 2:
-				System.out.println("no se ingresaran los datos del coche " + (i + 1));
-				coches[i] = coche;
+				System.out.println("no se ingresaran los datos del coche ");
+				coches[contador] = coche;
+				contador++;
 				break;
 			default:
 				System.out.println("esa opcion no es valida, porfavor vuelva a escribir su opcion");
-				break;
-			}
+				if (contador == 0) {
+					contador = 0;
+				} else {
+					contador = (contador - 1);
+				}
 
-		} // for
+				break;
+
+			}
+		} while (contador != coches.length);
+
+//		for (int i = 0; i < coches.length; i++) {
+//			Coche coche = new Coche();
+//			System.out.println("Si desea ingresar sus datos porfavor escriba 1, en caso contrario escriba 2");
+//			respuesta = Integer.parseInt(sc.nextLine());
+//
+//			switch (respuesta) {
+//
+//			case 1:
+//				System.out.println("Porfavor indique la matricula");
+//				coche.setMatricula(sc.nextLine());
+//
+//				System.out.println("Porfavor indique la marca");
+//				coche.setMarca(sc.nextLine());
+//
+//				System.out.println("Porfavor indique la modelo");
+//				coche.setModelo(sc.nextLine());
+//
+//				System.out.println("Porfavor indique la color");
+//				coche.setColor(sc.nextLine());
+//
+//				System.out.println("Porfavor indique la potencia");
+//				coche.setPotencia(Float.parseFloat(sc.nextLine()));
+//
+//				System.out.println("Porfavor indique la cilindrada");
+//				coche.setCilindrada(Float.parseFloat(sc.nextLine()));
+//
+//				coches[i] = coche;
+//				break;
+//
+//			case 2:
+//				System.out.println("no se ingresaran los datos del coche " + (i + 1));
+//				coches[i] = coche;
+//				break;
+//			default:
+//				System.out.println("esa opcion no es valida, porfavor vuelva a escribir su opcion");
+//				break;
+//			}
+//
+//		} // for
 
 		System.out.println("sus coches y caracteristicas ingresadas son las siguientes: ");
 		for (int i = 0; i < coches.length; i++) {
